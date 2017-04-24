b1f8d1ee7e2526a2b26647f96cfefb3f7681c0a4
HxiaoZ <huxiaozhong6030@163.com>
2017-04-24 11:17:21 +0800

    -

--- index.js
+++ index.js
@@ -3,66 +3,53 @@ import XzComponents from './src/components'
 import './css/main.css';
 var React = require("react");
 
+function NoAnimation(routeStack,pages){
+	for(var i=0,j=routeStack.length;i<j;i++){
+		var _key = routeStack[i]._key+"_wrapper";
+		var instance = routeStack[i].page;
+		if(i===j-1){
+			pages.push(<div className='xz-page-route-wrapper' key={_key}>{instance}</div>);
+	    }else{
+	    	pages.push(<div className='xz-page-route-wrapper'  style={{left:"-120%"}} key={_key}>{instance}</div>);
+
+	    }
+	}
+}
+
+function GoPreOrNext(lastClass,preClass,len,routeStack,pages){
+	for(var i=0;i<len;i++){
+		var _key = routeStack[i]._key+"_wrapper";
+		var instance = routeStack[i].page;
+		if(i===len-1){
+    		pages.push(<div className={lastClass} key={_key}>{instance}</div>);
+		}else if(i===len-2){
+			pages.push(<div className={preClass} key={_key}>{instance}</div>);
+		}else{
+			pages.push(<div className='xz-page-route-wrapper' key={_key} style={{left:"-120%"}}>{instance}</div>);
+		}
+	}
+}
+
 export default {
-	"pagecontainer":"xxxxxxx",
 	"Navigation":{
 		start(config){
 			Navigation.start(config,(routeStack,action,animationAction)=>{
-				
 				var pages = [];
 				var len = routeStack.length;
 			    console.log(animationAction+"	"+"	"+action+"	"+len);
 
 			   	if(len>1){
 			   		if(animationAction==='前进'){
-						for(var i=0;i<len;i++){
-							var _key = routeStack[i]._key+"_wrapper";
-							var instance = routeStack[i].page;
-							if(i===len-1){
-				        		pages.push(<div className='xz-page-route-wrapper right-in' key={_key}>{instance}</div>);
-							}else if(i===len-2){
-								pages.push(<div className='xz-page-route-wrapper left-out' key={_key}>{instance}</div>);
-							}else{
-								pages.push(<div className='xz-page-route-wrapper' key={_key} style={{left:"-120%"}}>{instance}</div>);
-							}
-				   		}
+			   			GoPreOrNext('xz-page-route-wrapper right-in','xz-page-route-wrapper left-out',len,routeStack,pages);
 			   		}else if(animationAction==="后退删除最后"){
-			   			for(var i=0;i<len;i++){
-							var _key = routeStack[i]._key+"_wrapper";
-							var instance = routeStack[i].page;
-							if(i===len-1){
-				        		pages.push(<div className='xz-page-route-wrapper right-out' key={_key}>{instance}</div>);
-							}else if(i===len-2){
-								pages.push(<div className='xz-page-route-wrapper left-in' key={_key}>{instance}</div>);
-							}else{
-								pages.push(<div className='xz-page-route-wrapper' key={_key} style={{left:"-120%"}}>{instance}</div>);
-							}
-				   		}
+				   		GoPreOrNext('xz-page-route-wrapper right-out','xz-page-route-wrapper left-in',len,routeStack,pages);
 						routeStack.splice(routeStack.length-1,1)
 			   		}else{
-			   			for(var i=0;i<len;i++){
-				   			var _key = routeStack[i]._key+"_wrapper";
-							var instance = routeStack[i].page;
-							if(i===len-1){
-				        		pages.push(<div className='xz-page-route-wrapper' key={_key}>{instance}</div>);
-					        }else{
-					        	pages.push(<div className='xz-page-route-wrapper'  style={{left:"-120%"}} key={_key}>{instance}</div>);
-
-					        }
-				   		}
+			   			NoAnimation(routeStack,pages);
 			   		}
 
 			   	}else{
-			   		for(var i=0;i<len;i++){
-			   			var _key = routeStack[i]._key+"_wrapper";
-						var instance = routeStack[i].page;
-						if(i===len-1){
-			        		pages.push(<div className='xz-page-route-wrapper' key={_key}>{instance}</div>);
-				        }else{
-				        	pages.push(<div className='xz-page-route-wrapper'  style={{left:"-120%"}} key={_key}>{instance}</div>);
-
-				        }
-			   		}
+			   		NoAnimation(routeStack,pages);
 			   	}
 			    
 			    return pages;
