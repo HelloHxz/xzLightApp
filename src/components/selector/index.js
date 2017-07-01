7e382c3eb6b4617d5e122d1ad35f99b945930b84
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2017-07-01 21:10:04 +0800

    -

--- src/components/selector/index.js
+++ src/components/selector/index.js
@@ -3,18 +3,32 @@ import "./index.less"
 import Style from "../../../utils/style"
 
 
-
+var data = [
+  {key:"11",value:"zhon",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"},
+  {key:"11",value:"xxx",pkey:"0"}
+];
 class SelectorColumn extends React.Component{
    constructor(props) {
     super(props)
     props.parent.instanceDict[props.pkey] = this;
     this.tranDict = Style.getTransitionKeys();
     this.state={
-      offset:0
+      offset:0,
+      data:data
     };
+    this.bottomLimit = 0-this.props.itemHeight*(this.state.data.length-1);
   }
 
   onTouchStart(e){
+    this.diff = 0;
     this.startY = e.touches[0].pageY;
     this.curOffset = this.state.offset;
     if(this.scrollEngine){
@@ -26,14 +40,20 @@ class SelectorColumn extends React.Component{
     this.curY = e.touches[0].pageY;
     this.diff = this.curY - this.startY;
     var offset = this.curOffset+this.diff;
+    if(offset>0||this.state.offset<this.bottomLimit ){
+      offset = this.curOffset+this.diff/3;
+    }
     this.setState({
       offset:offset
     });
   }
 
   onTouchEnd(e){
-    var t=0,b=this.state.offset,c=300,d=30;
-      this.scrollEngine=  Style.run(t, b, c, d);
+    if(this.diff===0){
+      return;
+    }
+    var t=0,b=this.state.offset,d=30;
+      this.scrollEngine=  Style.run(t, b, this.getCanScrollDistance(), d);
       this.scrollEngine.start((val)=>{
         this.setState({offset:val});
       },null,()=>{
@@ -41,18 +61,37 @@ class SelectorColumn extends React.Component{
       });
   }
 
+  getCanScrollDistance(){
+    var maxLen = 600;
+    if(this.state.offset>0){
+      return 0-this.state.offset;
+    }
+    if(this.state.offset<this.bottomLimit){
+      return this.bottomLimit-this.state.offset;
+    }
+    var len = 0 ;
+    if(this.diff>0){
+       len = (0-this.state.offset)<=maxLen?(0-this.state.offset):maxLen;
+    }
+    if(this.diff<0){
+      len = (this.bottomLimit-this.state.offset)<(0-maxLen)?(0-maxLen):(this.bottomLimit-this.state.offset);
+    }
+    return len;
+  }
+
  
 
 
   render() {
     var ty = {};
+    var child = [];
+    for(var i=0,j=this.state.data.length;i<j;i++){
+      var itemdata = this.state.data[i];
+      child.push(<li key={i}>{itemdata.value}</li>);
+    }
     ty[this.tranDict.transform] = "translate3d(0,"+(this.props.itemHeight*2+this.state.offset)+"px,0)";
     return (<ul style={ty} className='xz-selector-col'>
-        <li>xx</li>
-        <li>xx</li>
-        <li>xx</li>
-        <li>xx</li>
-        <li>xx</li>
+       {child}
       </ul>);
   }
 }
